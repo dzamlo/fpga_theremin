c2c98c6bbd67eb757ab3c5e5acbab74e7cf16660
Loïc Damien <loic.damien@dzamlo.ch>
2015-05-11 23:59:43 +0200

    Fix formatting issue

--- src/gen_lut.py
+++ src/gen_lut.py
@@ -21,27 +21,31 @@ def write_entity(f, entity_name, addr_bits, data_bits):
                                    data_bits=data_bits)
     f.write(entity_str)
 
+
 def bin_with_length(v, length):
     bits = bin(v)[2:]
-    return "0" * (length-len(bits)) + bits
+    return "0" * (length - len(bits)) + bits
+
 
 def write_architecture(f, entity_name, values, addr_bits, data_bits):
     architecture_str_start = "architecture lut_arch of {entity_name} is\n"\
                              "begin\n" \
                              "with addr select data <=\n"
-    architecture_str_start = architecture_str_start.format(entity_name=entity_name)     
+    architecture_str_start = architecture_str_start.format(
+        entity_name=entity_name)
     f.write(architecture_str_start)
-    for i,value in enumerate(values):
+    for i, value in enumerate(values):
         line_str = "to_signed({value}, {data_bits}) when \"{addr_bin}\",\n"
         line_str = line_str.format(value=value,
                                    data_bits=data_bits,
                                    addr_bin=bin_with_length(i, addr_bits))
         f.write(line_str)
-    
+
     architecture_str_end = "to_signed(0, {data_bits}) when others;\n"\
                            "end lut_arch;\n"
-    architecture_str_end = architecture_str_end.format(data_bits = data_bits);
+    architecture_str_end = architecture_str_end.format(data_bits=data_bits)
     f.write(architecture_str_end)
+
 
 def make_vhdl(filename, entity_name, f, addr_bits, data_bits):
     n = 2**addr_bits
